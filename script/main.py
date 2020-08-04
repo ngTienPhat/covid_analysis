83d752f2a183bd4fd7b755128585b98da367f73f
Phat T. Nguyen <1712122@student.hcmus.edu.vn>
2020-08-04 10:09:55 +0700

    delete redundant functions

--- script/main.py
+++ script/main.py
@@ -69,51 +69,6 @@ def test_basic_SIR(raw_df, attribute2fix: str):
     basic_sir.fit_single_attribute(attribute='R', visualize=True)
 
 
-def main(cfg):
-
-    # -----------------------------------------------------------
-    # TIME SIR:
-    data_dir = os.path.join(cfg.cwd, cfg.data.root)
-    file_dir = os.path.join(data_dir, STATE+'.csv')
-    
-    raw_df = pd.read_csv(file_dir)
-    
-    train_df, val_df = train_test_split(raw_df, 0.1)
-    cfg.model.predict_day = len(val_df)
-
-    # train_params = prepare_data(train_df['Active'].values, 
-    #                                 train_df['Confirmed'].values,
-    #                                 train_df['Deaths'].values,
-    #                                 train_df['Recovered'].values,
-    #                                 cfg.population.texas)
-
-    # val_parms = prepare_data(val_df['Active'].values, 
-    #                                 val_df['Confirmed'].values,
-    #                                 val_df['Deaths'].values,
-    #                                 val_df['Recovered'].values,
-    #                                 cfg.population.texas)
-    
-    raw_params = prepare_data(raw_df['Active'].values, 
-                                    raw_df['Confirmed'].values,
-                                    raw_df['Deaths'].values,
-                                    raw_df['Recovered'].values,
-                                    cfg.population.texas)
-
-    # model = TimeSIR(cfg, train_params)
-    # model.train()
-    # res = model.predict(val_parms)
-    # visualize_result(raw_params, res)
-
-    # plot_single_set(raw_params)
-
-
-    # -----------------------------------------------------------
-    # BASIC SIR:
-
-    basic_sir = BasicSIR(cfg, raw_params)
-    basic_sir.fit_single_attribute(attribute='R', visualize=True)
-
-
 if __name__ == "__main__":
     train_df, val_df, raw_df = load_data()
     
